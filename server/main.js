bbe6d89143d99a0cd81d50879258565297efcfd5
Alex Barry <alex@rekindleapp.com>
2016-09-11 21:14:09 -0400

    Enable jsx

--- server/main.js
+++ server/main.js
@@ -1,7 +1,8 @@
 var express = require('express');
-
 var app = new express();
 
 app.get('/', function(req,res){
-  res.render('./../app/index.ejs', {});
-}).listen(7777);
+  res.render('./../app/index.ejs');
+})
+.use(express.static(__dirname + './../app'))
+.listen(7777);
